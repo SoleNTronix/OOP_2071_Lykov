733a23f8fdca9a454c9f369fcc8e0330f3ac1131
SoleNTronix <kllykov@outlook.com>
2021-11-11 15:43:52 +0300

    New methods was created, class was renamed

--- Rational/Rational/Form1.cs
+++ Rational/Rational/Form1.cs
@@ -16,85 +16,66 @@ namespace Rational
         {
             InitializeComponent();            
         }
-
+        MyRational first;
+        MyRational second;
         private void Sum_Click(object sender, EventArgs e)
         {
-            Fraction first = new Fraction();
-            Fraction second = new Fraction();
-
-            bool check_num1 = int.TryParse(num1.Text, out first.numerator);
-            bool check_denum1 = int.TryParse(denum1.Text, out first.denumerator);
-            bool check_num2 = int.TryParse(num2.Text, out second.numerator);
-            bool check_denum2 = int.TryParse(denum2.Text, out second.denumerator);
-
-            if (!check_num1 || !check_num2 || !check_denum1 || !check_denum2)
-                MessageBox.Show("Ошибка ввода! Введите целое(ые) число(а)");
-            
-            Fraction result = new Fraction();
+            first = new MyRational();
+            second = new MyRational();
+            Check();
+            MyRational result = new MyRational();
             result.Sum(first, second);
-            resultNum.Text = Convert.ToString(result.numerator);
-            resultDenum.Text = Convert.ToString(result.denumerator);
+            Result(result);
             operation.Text = "+";
         }
 
         private void Dif_Click(object sender, EventArgs e)
         {
-            Fraction first = new Fraction();
-            Fraction second = new Fraction();
-
-            bool check_num1 = int.TryParse(num1.Text, out first.numerator);
-            bool check_denum1 = int.TryParse(denum1.Text, out first.denumerator);
-            bool check_num2 = int.TryParse(num2.Text, out second.numerator);
-            bool check_denum2 = int.TryParse(denum2.Text, out second.denumerator);
-
-            if (!check_num1 || !check_num2 || !check_denum1 || !check_denum2)
-                MessageBox.Show("Ошибка ввода! Введите целое(ые) число(а)");
-
-            Fraction result = new Fraction();
+            first = new MyRational();
+            second = new MyRational();
+            Check();
+            MyRational result = new MyRational();
             result.Dif(first, second);
-            resultNum.Text = Convert.ToString(result.numerator);
-            resultDenum.Text = Convert.ToString(result.denumerator);
+            Result(result);
             operation.Text = "-";
         }
 
         private void Mult_Click(object sender, EventArgs e)
         {
-            Fraction first = new Fraction();
-            Fraction second = new Fraction();
-
-            bool check_num1 = int.TryParse(num1.Text, out first.numerator);
-            bool check_denum1 = int.TryParse(denum1.Text, out first.denumerator);
-            bool check_num2 = int.TryParse(num2.Text, out second.numerator);
-            bool check_denum2 = int.TryParse(denum2.Text, out second.denumerator);
-
-            if (!check_num1 || !check_num2 || !check_denum1 || !check_denum2)
-                MessageBox.Show("Ошибка ввода! Введите целое(ые) число(а)");
-
-            Fraction result = new Fraction();
+            first = new MyRational();
+            second = new MyRational();
+            Check();
+            MyRational result = new MyRational();
             result.Mult(first, second);
-            resultNum.Text = Convert.ToString(result.numerator);
-            resultDenum.Text = Convert.ToString(result.denumerator);
+            Result(result);
             operation.Text = "*";
         }
 
         private void Div_Click(object sender, EventArgs e)
         {
-            Fraction first = new Fraction();
-            Fraction second = new Fraction();
+            first = new MyRational();
+            second = new MyRational();
+            Check();
+            MyRational result = new MyRational();
+            result.Div(first, second);
+            Result(result);
+            operation.Text = "/";
+        }
 
+        private void Check()
+        {
             bool check_num1 = int.TryParse(num1.Text, out first.numerator);
             bool check_denum1 = int.TryParse(denum1.Text, out first.denumerator);
             bool check_num2 = int.TryParse(num2.Text, out second.numerator);
             bool check_denum2 = int.TryParse(denum2.Text, out second.denumerator);
 
             if (!check_num1 || !check_num2 || !check_denum1 || !check_denum2)
-                MessageBox.Show("Ошибка ввода! Введите целое(ые) число(а)");
-
-            Fraction result = new Fraction();
-            result.Div(first, second);
+                return;
+        }
+        private void Result(MyRational result)
+        {
             resultNum.Text = Convert.ToString(result.numerator);
             resultDenum.Text = Convert.ToString(result.denumerator);
-            operation.Text = "/";
         }
     }
     
--- Rational/Rational/MyRational.cs
+++ Rational/Rational/MyRational.cs
@@ -6,48 +6,48 @@ using System.Threading.Tasks;
 
 namespace Rational
 {
-    public class Fraction
+    public class MyRational
     {
         public int numerator;
         public int denumerator;
-        public void Sum(Fraction one, Fraction two)
+        public void Sum(MyRational one, MyRational two)
         {
             int sum_denum = one.denumerator * two.denumerator;
             int sum_num = one.numerator * two.denumerator + two.numerator * one.denumerator;
-            Fraction res = new Fraction();
+            MyRational res = new MyRational();
             res = Red(sum_num, sum_denum);
             numerator = res.numerator;
             denumerator = res.denumerator;
         }
-        public void Dif(Fraction one, Fraction two)
+        public void Dif(MyRational one, MyRational two)
         {
             int dif_denum = one.denumerator * two.denumerator;
             int dif_num = one.numerator * two.denumerator - two.numerator * one.denumerator;
-            Fraction res = new Fraction();
+            MyRational res = new MyRational();
             res = Red(dif_num, dif_denum);
             numerator = res.numerator;
             denumerator = res.denumerator;
         }
-        public void Mult(Fraction one, Fraction two)
+        public void Mult(MyRational one, MyRational two)
         {
             int mult_num = one.numerator * two.numerator;
             int mult_denum = one.denumerator * two.denumerator;
-            Fraction res = new Fraction();
+            MyRational res = new MyRational();
             res = Red(mult_num, mult_denum);
             numerator = res.numerator;
             denumerator = res.denumerator;
         }
-        public void Div(Fraction one, Fraction two)
+        public void Div(MyRational one, MyRational two)
         {
             int div_num = one.numerator * two.denumerator;
             int div_denum = one.denumerator * two.numerator;
-            Fraction res = new Fraction();
+            MyRational res = new MyRational();
             res = Red(div_num, div_denum);
             numerator = res.numerator;
             denumerator = res.denumerator;
         }
 
-        Fraction Red(int n, int d)
+        MyRational Red(int n, int d)
         {
             int n_nod = n;
             int d_nod = d;
@@ -60,7 +60,7 @@ namespace Rational
             }
             n = n / d_nod;
             d = d / d_nod;
-            Fraction res = new Fraction();
+            MyRational res = new MyRational();
             res.numerator = n;
             res.denumerator = d;
             return res;
